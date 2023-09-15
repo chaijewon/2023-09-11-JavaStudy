8b03b60adeff6b0fab90322c32bb3c6f135411cd
SIST <SIST@DESKTOP-IPQ8TQC>
2023-09-15 12:50:42 +0900

    1111

---
+++ Chapter03_제어문/src/단일조건문_1.java
@@ -0,0 +1,17 @@
+// 알파벳을 받아서 대문자,소문자,숫자,다른 문자 
+import java.util.Scanner;
+public class 단일조건문_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scan=new Scanner(System.in);
+        // 알파벳 받기 
+        System.out.print("알파벳 입력:");
+        
+        // 대문자
+        // 소문자 
+        // 숫자
+        // 기타 
+	}
+
+}
